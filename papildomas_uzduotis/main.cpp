9217405ac24a39944a97d4bdd27b82b58922cf7f
Veronika <veronika.rimkeviciute@gmail.com>
2025-12-23 15:48:45 +0200

    Initial commit

---
+++ papildomas_uzduotis/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  papildomas_uzduotis
+//
+//  Created by Veronika  on 23/12/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
